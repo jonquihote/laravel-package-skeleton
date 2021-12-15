d6156597e5997974e89c99216c4f8163f8f728d2
Joni Chandra <jonquihote@gmail.com>
2021-12-15 15:09:18 +0700

    refactor: set default test case

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -2,10 +2,7 @@
 
 namespace Tests;
 
-use Tests\Testing\TestCase;
 use Masonry\ServiceProvider;
-
-uses(TestCase::class);
 
 it('ensures that the service provider is loaded', function () {
     $providerIsLoaded = $this->app->providerIsLoaded(ServiceProvider::class);
